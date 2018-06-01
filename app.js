fce2cc027a43b590f26174dfa704f62e4685167e
raniaSayed <raniasayed313@gmail.com>
2018-06-01 04:20:39 +0200

    intialize reservation controller and model, import them into app.js

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ var mongoose = require("mongoose");
 //connect to mongodb
 mongoose.connect("mongodb://localhost:27017/fligth_reservation313");
 
+//register models
+fs.readdirSync(path.join(__dirname,"models")).forEach(function (filename) {
+    require("./models/"+filename);
+});
+
 //api body-parser middleware
 app.use(bodyParser.json());
+
+var reservation = require('./controllers/reservation');
+app.use('/reservation',reservation);
 
 //start listenning
 app.listen(9090,function(){
---
+++ controllers/reservation.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var mongoose =  require('mongoose');
+var fs = require("fs");
+
+
+module.exports = router;
